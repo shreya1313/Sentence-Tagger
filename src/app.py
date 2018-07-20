99d1b9aba97ec877d386cff00b8553cffadd0cd3
Arpit Goyal <arpitgoyal.thunderbird@gmail.com>
2018-07-20 15:54:49 +0530

    fully functional flask application incorporated

--- src/app.py
+++ src/app.py
@@ -19,3 +19,11 @@ mongoengine.connect(
 
 for cli_name, cli_command in ALL_CLI_COMMANDS.items():
     application.cli.add_command(cli_command, name=cli_name)
+
+
+with application.app_context():
+    from apis.urls import api_urls
+
+    # Adding all the url rules in the api application
+    for url, view, methods, _ in api_urls:
+        application.add_url_rule(url, view_func=view, methods=methods)
