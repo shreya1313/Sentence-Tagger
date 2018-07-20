90b2feaec7e79ae009678efecf1910d74b8a57db
Arpit Goyal <arpitgoyal.thunderbird@gmail.com>
2018-07-20 15:08:56 +0530

    updated the scaffolding to use common submodule

--- src/settings/local_settings.py
+++ src/settings/local_settings.py
@@ -1,1 +1,10 @@
 from common.settings.common_settings import *
+import os
+
+
+BASE_PATH = os.path.dirname(os.path.dirname(__file__))
+
+PROTOFILES_DIRECTORY = os.path.join(BASE_PATH, 'protobuf')
+DESCRIPTORS_DIRECTORY = os.path.join(BASE_PATH, 'descriptors')
+
+EXTERNAL_PROTOBUF_CONFIG = []
