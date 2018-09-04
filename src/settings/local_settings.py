514060f163453389006452496dd95f522bd12b36
Arpit Goyal <arpitgoyal.thunderbird@gmail.com>
2018-09-04 20:52:32 +0530

    updated the skeleton repository

--- src/settings/local_settings.py
+++ src/settings/local_settings.py
@@ -3,13 +3,11 @@ import os
 
 
 BASE_PATH = os.path.dirname(os.path.dirname(__file__))
-
-PROTOFILES_DIRECTORY = os.path.join(BASE_PATH, 'protobuf')
-DESCRIPTORS_DIRECTORY = os.path.join(BASE_PATH, 'descriptors')
-
-EXTERNAL_PROTOBUF_CONFIG = []
-
 DEBUG = False
+
+# Protofiles Settings
+PROTOFILES_DIRECTORY = os.path.join(BASE_PATH, 'protobuf')
+PROTOFILES_NEEDED += []
 
 # ElasticSearch Configuration
 USE_ES = False
